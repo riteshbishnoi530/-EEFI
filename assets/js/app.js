d9c1b00bd5692a9b2b62a6d8b254798241489729
riteshbishnoi530 <bishnoiritesh6@gmail.com>
2024-09-25 12:33:12 +0530

    roadmap cards text alignment

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,6 +5,6 @@ const navmenu = () => {
     document.getElementById("nav_line_2").classList.toggle("-rotate-45");
     document
       .getElementById("nav_line_out_side")
-      .classList.toggle("translate-x-6");
+      .classList.toggle("translate-x-9");
     document.body.classList.toggle("overflow-hidden");
   };
